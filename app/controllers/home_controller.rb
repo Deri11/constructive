ed99a1520522d4e074a3a3840ae9456f8af7a702
Deven Blackburn <deven@codefll.org>
2016-05-23 09:44:54 -0400

    content added for demo conference call

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,18 @@
 class HomeController < ApplicationController
 
   def index
+    @shots = Shot.all
   end
 
+
 end
+
+#   <ul>
+#     <% @shots.each do |s| %>
+#       <li><img src='<%#=s.(id: 2434).images_normal%>'></li>
+#     <%end%>
+#   </ul>
+#
+#
+# <h1> this is the example </h1>
+# <img src='<%#= @shots.select {|s| (s.id==2434).images_normal } %>'>
