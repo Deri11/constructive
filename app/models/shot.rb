63720dbf8b545c2fd9bdf44b7597a097621dc6f0
Deven Blackburn <deven@codefll.org>
2016-05-27 23:49:52 -0400

    shot tags added as search param

--- app/models/shot.rb
+++ app/models/shot.rb
@@ -2,8 +2,8 @@ class Shot < ActiveRecord::Base
 belongs_to :user
 has_many :comments
 
-  def self.search(query)
-    where("title LIKE ? OR description LIKE ?", ''"%#{query}%"'', ''"%#{query}%"'')
+  def self.search(query, options={})
+    where("title LIKE ? OR description LIKE ? OR tags LIKE ?", ''"%#{query}%"'', ''"%#{query}%"'', ''"%#{query}%"'')
   end
 
 end
